ad1f9819e14b30c6c99f3ca366f07708806f661d
Madigan Kim <ys.madigankim@gmail.com>
2015-06-21 17:57:14 -0400

    New rails project, with User/Team/Member templates.

---
+++ whyvote/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render(:file => File.join(Rails.root, 'public/404.html'), :status => 404, :layout =>false)
+      return
+    end
+  end
+end
