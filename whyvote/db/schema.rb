14a3f77e66864b49dd2f0391cd693f68be63dc6e
Madigan Kim <ys.madigankim@gmail.com>
2015-06-22 16:25:32 -0400

    Added fields to Users/Teams

--- whyvote/db/schema.rb
+++ whyvote/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621201853) do
+ActiveRecord::Schema.define(version: 20150622202322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,9 +28,10 @@ ActiveRecord::Schema.define(version: 20150621201853) do
   add_index "members", ["user_id"], name: "index_members_on_user_id", using: :btree
 
   create_table "teams", force: :cascade do |t|
-    t.string   "name",       default: "Team Name", null: false
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.string   "name",        default: "Team Name", null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,6 +49,10 @@ ActiveRecord::Schema.define(version: 20150621201853) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "school"
+    t.string   "degree"
+    t.text     "description"
+    t.string   "graduation_date"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
