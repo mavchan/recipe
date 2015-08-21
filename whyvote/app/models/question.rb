9d572560e6c69c5c3faf5b589d6e52d8220155d6
Madigan Kim <ys.madigankim@gmail.com>
2015-08-21 18:15:54 -0400

    Removed old opinions fields

--- whyvote/app/models/question.rb
+++ whyvote/app/models/question.rb
@@ -5,19 +5,17 @@ class Question < ActiveRecord::Base
       :reject_if => ->(a) { a[:text].blank? },
       :allow_destroy => true
 
-  enum type: [ :selection, :rank ]
-
-  validates_inclusion_of :type, :in => Question.types
-
-  def type_enum
-    self.class.types.to_a
-  end
-
-  def type= value
-    if value.kind_of?(String) and value.to_i.to_s == value
-      super value.to_i
-    else
-      super value
-    end
-  end
+  # enum type: [ :selection, :rank ]
+  #
+  # def type_enum
+  #   self.class.types.to_a
+  # end
+  #
+  # def type= value
+  #   if value.kind_of?(String) and value.to_i.to_s == value
+  #     super value.to_i
+  #   else
+  #     super value
+  #   end
+  # end
 end
